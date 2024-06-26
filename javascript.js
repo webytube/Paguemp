c9d2a8ff9b1da6037a9208b2d02b1eee121e1eb1
webytube <contatochegamaisgamer@gmail.com>
2024-06-26 08:40:06 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-const ACCESS_TOKEN = 'APP_USR-7683778213528282-061306-ca5abb469f91bd4e26734d1a1f3f0dd9-1728455248';
+const ACCESS_TOKEN = 'TEST-7683778213528282-061306-0afbdb004e7fe9a646c6be96c0d228ac-1728455248';
 
 fetch('https://api.mercadopago.com/checkout/preferences', {
     method: 'POST',
@@ -15,7 +15,7 @@ fetch('https://api.mercadopago.com/checkout/preferences', {
             quantity: 1,
             unit_price: 01
         }],
-        auto_return: 'success',
+        auto_return: 'approved',
         back_urls: {
             success: 'http://seusite.com/success',
             pending: 'http://seusite.com/pending',
